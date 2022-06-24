a009ccd2c48c9f581c28b1729d39dba8a6c122c2
k.shaigan <k.shaigan@narvanstartupstudio.com>
2022-06-24 18:07:27 +0430

    print longitude and latitude

--- app/src/main/java/com/example/nearby/MainActivity.kt
+++ app/src/main/java/com/example/nearby/MainActivity.kt
@@ -2,23 +2,23 @@ package com.example.nearby
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
-import android.location.LocationRequest
 import android.os.Bundle
-import android.util.Log
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,33 +26,40 @@ import androidx.core.app.ActivityCompat
 import com.example.nearby.ui.theme.NearbyTheme
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-
+import androidx.compose.runtime.*
 
 class MainActivity : ComponentActivity() {
 
-    var lng = 0.0
-    var lat = 0.0
+private lateinit var fusedLocProviderClient : FusedLocationProviderClient
 
-    //unique number
-    var PERMISSION_ID = 1000
 
-    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    lateinit var locationRequest : LocationRequest
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        fusedLocProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        val lonLat: LongLat = getCurLocation()
+        println("LONG: ${lonLat.long} \n LAT: ${lonLat.lat} ")
+
         setContent {
-             MyApp{
-                 ScreenContent(locations = listOf(Location(title= "hi"), Location(title = "no")), modifier = Modifier.padding(8.dp))
-             }
+            MyApp {
+                ScreenContent(modifier = Modifier.padding(8.dp))
+            }
         }
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-    }
 
-    private fun getLastLocation(){
+
+
+
+    }
+    private fun getCurLocation() : LongLat {
+         var lon = 0.0
+         var lat = 0.0
+
+        if(checkPermissions()){
             if(isLocationEnabled()){
+                //final long and lat
                 if (ActivityCompat.checkSelfPermission(
                         this,
                         Manifest.permission.ACCESS_FINE_LOCATION
@@ -62,56 +69,57 @@ class MainActivity : ComponentActivity() {
                     ) != PackageManager.PERMISSION_GRANTED
                 ) {
                     requestPermission()
-                    return
+                    return LongLat(0.0, 0.0)
                 }
-                fusedLocationProviderClient.lastLocation.addOnCompleteListener{ task ->
-                    var location = task.result
+                fusedLocProviderClient.lastLocation.addOnCompleteListener(this){ task ->
+                    val location = task.result
                     if(location == null){
-
-
+                        Toast.makeText(this, "Received NULL!", Toast.LENGTH_SHORT).show()
                     }else{
-                        lng = location.longitude
-                        lat = location.latitude
-                        println("Location is lat: $lat , and long: $lng")
+                        Toast.makeText(this, "RECEIVED!", Toast.LENGTH_SHORT).show()
+                        println("LONG: ${location.longitude} \n LAT: ${location.latitude}")
                     }
+
 
                 }
 
             }else{
-                Toast.makeText(this, "Please enable location service", Toast.LENGTH_SHORT).show()
+                //setting open here
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
             }
+
+        }else{
+            //request permission here
+            requestPermission()
+        }
+        return LongLat(lon, lat)
     }
 
-    private fun getNewLocation(){
-//        locationRequest = LocationRequest()
-//        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-//        locationRequest.intervalMillis = 0
-//
-
-
+    private fun isLocationEnabled() : Boolean {
+        val locationManager : LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
 
     }
 
-    //Check users permission
-//    private fun CheckPermission(): Boolean {
-//        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-//            return true
-//        }
-//        return false
-//    }
-
-    //Get users permission
-    private fun requestPermission(){
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_ID
-        )
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ), PERMISSION_REQ_ACCESS_LOC)
     }
 
-    //Check location service
-    private fun isLocationEnabled():Boolean{
-        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    companion object {
+        private const val PERMISSION_REQ_ACCESS_LOC = 10
+
+    }
+
+    private fun checkPermissions(): Boolean{
+        return (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                ==PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                ==PackageManager.PERMISSION_GRANTED)
     }
 
     override fun onRequestPermissionsResult(
@@ -120,19 +128,26 @@ class MainActivity : ComponentActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == PERMISSION_ID){
-            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED) {
-                Log.d("Debug:", "You have the permissions")
+        if(requestCode == PERMISSION_REQ_ACCESS_LOC){
+            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(applicationContext, "Granted", Toast.LENGTH_SHORT).show()
+                getCurLocation()
+            }else{
+                Toast.makeText(applicationContext, "Denied!", Toast.LENGTH_SHORT).show()
+
             }
+
         }
     }
-
 
 }
 
 
+
+
 //Holds value of location details
 data class Location(val title: String)
+data class LongLat(val long: Double, val lat: Double)
 
 @Composable
 fun MyApp(content: @Composable ()-> Unit){
@@ -147,34 +162,31 @@ fun MyApp(content: @Composable ()-> Unit){
 }
 
 @Composable
-fun EachItem(location: Location){
-    Text(location.title, color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(8.dp))
+fun EachItem(loc: LongLat){
+    Column() {
+        Text("LONG: ${loc.long}", color = MaterialTheme.colors.primary,
+            modifier = Modifier.padding(8.dp))
+        Text("LAT: ${loc.lat}", color = MaterialTheme.colors.primary,
+            modifier = Modifier.padding(8.dp))
+    }
 }
 
 @Composable
-fun ScreenContent(locations: List<Location>, modifier: Modifier = Modifier){
-
-    LazyColumn(modifier = modifier){
-        items(items = locations){ item ->
-            EachItem(location = item)
-
-        }
-
+fun ScreenContent(modifier: Modifier = Modifier){
+//    val longlat : LongLat by remember {
+//        mutableStateOf(LongLat(0.0,0.0))
+//    }
+    Column(modifier = modifier){
+//        EachItem(loc = loc)
     }
 
 
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApp{
-        ScreenContent(locations = listOf(Location(title= "hi"), Location(title = "no")), modifier = Modifier.padding(8.dp))
+        ScreenContent(modifier = Modifier.padding(8.dp))
     }
 }
